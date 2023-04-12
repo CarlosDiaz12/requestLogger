7a7c42c7b4c6bc10621946d985303949d053fe9a
Carlos Diaz <carlos.cedp@gmail.com>
2023-04-12 18:09:20 -0400

    delete routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoggerServicesComponent } from './components/logger-services/logger-services.component';
 import { NgxPaginationModule } from 'ngx-pagination';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoggerServicesComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    NgxPaginationModule,
-    HttpClientModule,
-    FormsModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, NgxPaginationModule, HttpClientModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
